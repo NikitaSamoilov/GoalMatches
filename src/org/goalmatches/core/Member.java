b2e2d88f75b1956fe7a513f51c95aa900772f918
NikitaSamoilov <nswork1987@gmail.com>
2015-05-06 21:56:41 +0300

    Add history lists for push and pop count at member

--- src/org/goalmatches/core/Member.java
+++ src/org/goalmatches/core/Member.java
@@ -1,9 +1,14 @@
 package org.goalmatches.core;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Member {
     private String name;
     private int currentValue;
+    private List<Integer> historyPush = new ArrayList<Integer>();
+    private List<Integer> historyPop = new ArrayList<Integer>();
 
     public String getName() {
         return name;
@@ -19,15 +24,16 @@ public class Member {
 
     public void pushValue(int count) {
         currentValue += count;
+        historyPush.add(count);
     }
 
     public void popValue(int count) {
         if (currentValue < count) {
             currentValue = 0;
+            historyPop.add(currentValue);
         } else {
             currentValue -= count;
+            historyPop.add(count);
         }
-
-
     }
 }
