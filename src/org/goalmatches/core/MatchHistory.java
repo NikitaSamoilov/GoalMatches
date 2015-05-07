c9574756e8d626ba3b2a5235d4e9b5f36e459a92
NikitaSamoilov <nswork1987@gmail.com>
2015-05-07 18:40:28 +0300

    Balance printing

--- src/org/goalmatches/core/MatchHistory.java
+++ src/org/goalmatches/core/MatchHistory.java
@@ -7,12 +7,24 @@ public class MatchHistory {
     private List<Match> pushHistory = new ArrayList<Match>();
     private List<Match> popHistory = new ArrayList<Match>();
 
+
     public List<Match> getPushHistory() {
         return pushHistory;
     }
 
     public List<Match> getPopHistory() {
         return popHistory;
+    }
+
+    public List<Match> calculateBalanceHistory() {
+        List<Match> balanceHistory = new ArrayList<Match>();
+        int balance = 0;
+        for (int i = 0; i < pushHistory.size(); i++) {
+            balance += (pushHistory.get(i).getValue() - popHistory.get(i).getValue());
+            balanceHistory.add(new Match(balance));
+        }
+
+        return balanceHistory;
     }
 
     @Override
@@ -24,6 +36,12 @@ public class MatchHistory {
         }
         strBuilder.append("\nPop History:");
         for (Match match : popHistory) {
+            strBuilder.append(String.format(" %d", match.getValue()));
+        }
+
+        List<Match> balanceHistory = calculateBalanceHistory();
+        strBuilder.append("\nBalance:");
+        for (Match match : balanceHistory) {
             strBuilder.append(String.format(" %d", match.getValue()));
         }
 
